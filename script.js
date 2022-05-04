cd04864686caf006fef069125a872cd44084ae6a
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-04 22:50:10 +0530

    funtion to change image

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function run() {
   idx++;
   changeImage();
 }
+
+function changeImage() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
