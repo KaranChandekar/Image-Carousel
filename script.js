377e2d1283c2f6766121b9baceb5e722aeee8da8
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-04 22:49:22 +0530

    run function

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ const rightBtn = document.getElementById("right");
 
 const img = document.querySelectorAll("#imgs img");
 
+let idx = 0;
+
 let interval = setInterval(run, 2000);
+
+function run() {
+  idx++;
+  changeImage();
+}
